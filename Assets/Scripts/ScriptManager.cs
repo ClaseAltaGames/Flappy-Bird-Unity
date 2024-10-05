46778fb0f4fb7718763caa1131f43b4f136476fb
Pau Mena Torres <117993994+PauMenaTorres@users.noreply.github.com>
2024-10-05 12:45:32 +0200

    Flappy Bird 1.0
    Co-Authored-By: Edgar Mesa <118179227+edgarmd1@users.noreply.github.com>

---
+++ Assets/Scripts/ScriptManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScriptManager : MonoBehaviour
+{
+    public static ScriptManager instance;
+
+    [SerializeField] private GameObject _gameOverCanvas;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+        Time.timeScale = 1f;
+    }
+
+    public void GameOver()
+    {
+        _gameOverCanvas.SetActive(true);
+
+        Time.timeScale = 0f;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
